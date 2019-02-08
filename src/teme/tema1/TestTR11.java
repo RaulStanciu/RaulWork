ec7aa308edf789e6ab8902943f19bf086b9f1623
stanc <stanc@DESKTOP-1HOTBEA>
2019-02-08 14:40:52 +0200

    Tema1

---
+++ src/teme/tema1/TestTR11.java
@@ -0,0 +1,26 @@
+package teme.tema1;
+
+public class TestTR11 {
+	
+	public static boolean isEven(int n) {
+			
+		return n % 2 == 0;
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(isEven(0));
+		System.out.println(isEven(1));
+		System.out.println(isEven(2));
+		System.out.println(isEven(3));
+	}
+	
+	
+	
+	
+
+
+}
---
+++ src/teme/tema1/TestTR3.java
@@ -0,0 +1,23 @@
+package teme.tema1;
+
+import java.util.Scanner;
+
+public class TestTR3 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		
+		
+		System.out.println(number * number);
+		System.out.println(number * number * number);
+		
+		scanner.close();
+		
+		
+	}
+	
+	
+
+}
